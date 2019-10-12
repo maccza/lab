4c514540bcee95ff0fd7e7aa78d63c56565143c1
maccza <mc180996@wp.pl>
2019-10-12 13:14:47 +0200

    task3

--- lab2/task_3.py
+++ lab2/task_3.py
@@ -8,8 +8,15 @@ def unique(values):
     :return: Unique values in order of appear.
     :rtype: list
     """
-    pass
+   
+    unq = []
+    for i in values:
+        if i not in unq:
+            unq.append(i)
+    
+    return unq
 
 
 if __name__ == "__main__":
-    assert [1, 5, 3, 6, 7, 2, 4] == unique([1, 5, 3, 5, 6, 7, 2, 1, 4, 1, 5])
+   
+   assert [1, 5, 3, 6, 7, 2, 4] == unique([1, 5, 3, 5, 6, 7, 2, 1, 4, 1, 5])
